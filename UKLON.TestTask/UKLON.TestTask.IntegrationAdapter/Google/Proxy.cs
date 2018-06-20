4033d8bb13ee35d6f0d983ed7b1082a21d20c952
Sergey Ilminskyi <paintersv@gmail.com>
2018-06-21 00:07:16 +0300

    temp 2

--- UKLON.TestTask/UKLON.TestTask.IntegrationAdapter/Google/Proxy.cs
+++ UKLON.TestTask/UKLON.TestTask.IntegrationAdapter/Google/Proxy.cs
@@ -1,29 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Google.GData.Client;
-using Google.GData.Extensions;
-using Google.GData.Spreadsheets;
+using System.IO;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Services;
 
 namespace UKLON.TestTask.IntegrationAdapter.Google
 {
     public class Proxy
     {
+        static readonly string[] Scopes = { SheetsService.Scope.Spreadsheets };
+        static readonly string ApplicationName = "UKLON test";
+        static readonly string SpreadsheetId = "1AhemqFP2lZ4ifcXGmArOydA3w24Yd7LdQ3KZveN-JR4";
+        static readonly string sheet = "Traffic.Region.Data";
+        static SheetsService service;
+
         public void Temp()
         {
-            SpreadsheetsService myService = new SpreadsheetsService("exampleCo-exampleApp-1");
-            myService.setUserCredentials("pav@gmail.com", "");
 
-            SpreadsheetQuery query = new SpreadsheetQuery();
-            SpreadsheetFeed feed = myService.Query(query);
 
-            Console.WriteLine("Your spreadsheets: ");
-            foreach (SpreadsheetEntry entry in feed.Entries)
+            GoogleCredential credential;
+            using (var stream = new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
             {
-                Console.WriteLine(entry.Title.Text);
+                credential = GoogleCredential.FromStream(stream)
+                  .CreateScoped(Scopes);
             }
+
+            // Create Google Sheets API service.
+            service = new SheetsService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            var range = $"A:B";
+            SpreadsheetsResource.ValuesResource.GetRequest request =
+                    service.Spreadsheets.Values.Get(SpreadsheetId, range);
+
+            var response = request.Execute();
+            IList<IList<object>> values = response.Values;
+            if (values != null && values.Count > 0)
+            {
+                foreach (var row in values)
+                {
+                    // Print columns A to F, which correspond to indices 0 and 4.
+                    Console.WriteLine("{0} | {1}", row[0], row[1]);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No data found.");
+            }
+
         }
     }
 }
