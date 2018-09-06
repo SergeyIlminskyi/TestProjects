2023da0f04e668d2657f9301c119775031fb0153
PENTEGY-UA\sergey.ilminskyi <sergey.ilminskyi@pentegyservices.com>
2018-09-06 17:43:24 +0300

    Moneyveo base

---
+++ Moneyveo.TestTask/Moneyveo.TestTask/App_Start/BundleConfig.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Optimization;
+
+namespace Moneyveo.TestTask
+{ 
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/Scripts/jquery").Include(
+                "~/Scripts/jquery-{version}.js",
+                "~/Scripts/jquery.unobtrusive-ajax.js"
+            ));
+
+            bundles.Add(new StyleBundle("~/assets/css/styles").Include());
+        }
+    }
+}
