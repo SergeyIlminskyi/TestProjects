30a9c445bb5f2169e3e0286ac1e1548c9423d3f9
PENTEGY-UA\sergey.ilminskyi <sergey.ilminskyi@pentegyservices.com>
2018-09-06 17:52:36 +0300

    core test

---
+++ CoreTestApp/CoreTestApp/Middleware/RoutingMiddleware.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace CoreTestApp
+{
+    public class RouterMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public RouterMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            string path = context.Request.Path.Value.ToLower();
+
+            if (path == "/index")
+            {
+                await context.Response.WriteAsync("Home Page");
+            }
+            else if (path == "/about")
+            {
+                await context.Response.WriteAsync("About");
+            }
+            else
+            {
+                context.Response.StatusCode = 404;
+            }
+
+            await _next.Invoke(context);
+        }
+    }
+}
