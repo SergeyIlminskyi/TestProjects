887a149b8b58c46edc7c08e32cd52164962feede
PENTEGY-UA\sergey.ilminskyi <sergey.ilminskyi@pentegyservices.com>
2018-09-07 18:07:49 +0300

    temp

--- Moneyveo.TestTask/Moneyveo.TestTask/Controllers/HomeController.cs
+++ Moneyveo.TestTask/Moneyveo.TestTask/Controllers/HomeController.cs
@@ -8,9 +8,41 @@ namespace Moneyveo.TestTask.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly string matrixViewPath = "../Shared/HomePartialView/Matrix";
+
+
+        [AcceptVerbs(HttpVerbs.Get)]
         public ActionResult Index()
         {
             return View();
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult ImportMatrixFromFile(HttpPostedFileBase file)
+        {
+            return PartialView(matrixViewPath);
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult GenerateMatrix(int size)
+        {
+            return PartialView(matrixViewPath);
+        }
+
+        [AcceptVerbs(HttpVerbs.Get)]
+        public ActionResult RotateMatrix()
+        {
+            return PartialView(matrixViewPath);
+        }
+
+        [AcceptVerbs(HttpVerbs.Get)]
+        public FileContentResult ExportMatrixToFile()
+        {
+            using (var memoryStream = new System.IO.MemoryStream())
+            using (var streamWriter = new System.IO.StreamWriter(memoryStream))
+            {
+                return File(memoryStream.ToArray(), "text/csv", "ExportMatrix.csv");
+            }
         }
     }
 }
